05ba45f71f35a3fd96381ae2fc5d162f857bbe5e
Venom2879 <you@example.com>
2026-04-09 12:22:19 +0200

    fix csrf

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -9,8 +9,8 @@ class Database extends PDO
 {
     public function __construct()
     {
-        $username = 'formation';
-        $password = 'paris';
+        $username = 'jerome';
+        $password = 'password';
         $host = '127.0.0.1';
         $port = 3306;
         $dbName = 'php_poo';
